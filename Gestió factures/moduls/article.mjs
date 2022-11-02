2b7cc4c276d58fa411d077f75e1556aab58e0da3
itsnyly <82224972+itsnyly@users.noreply.github.com>
2022-11-02 18:48:20 +0100

    Add files via upload

---
+++ Gestió factures/moduls/article.mjs
@@ -0,0 +1,7 @@
+export default class Article{
+    constructor(nomArticle,preuArticle,codiArticle){
+        this.nomArticle = nomArticle;
+        this.preuArticle = preuArticle;
+        this.codiArticle = codiArticle;
+    }
+}
